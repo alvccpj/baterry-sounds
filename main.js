58b439917f3e532f1c8ae32fa06c152537647957
Álvaro Jordão <alvccpj@gmail.com>
2023-04-19 19:13:47 -0300

    2nd commit

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
-
-//document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-for (contador = 0; contador < listaDeTeclas.length; contador++) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
-    console.log(contador);
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code)
+
+        if (evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
-
-
-
-
